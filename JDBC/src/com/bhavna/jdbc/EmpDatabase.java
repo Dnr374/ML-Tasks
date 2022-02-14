0d92e0b8be1fe56fbde5043e504f28d970b01814
NarasimharaoDasari <Narasimha.Dasari@bhavnacorp.com>
2022-02-14 12:42:48 +0530

    JDBC

--- JDBC/src/com/bhavna/jdbc/EmpDatabase.java
+++ JDBC/src/com/bhavna/jdbc/EmpDatabase.java
@@ -12,6 +12,127 @@ public class EmpDatabase {
 	static PreparedStatement ps;
 	Scanner scanner =new Scanner(System.in);
 	
+	public void selectByOption(int option)
+	{
+		connectToDB();
+		Employee emp=new Employee();
+		char status = 'Y';
+		do {
+			switch (option) {
+			case 1: {
+				System.out.println("How many records do you want to enter");
+				int n = scanner.nextInt();
+				scanner.nextLine();
+				for(int i=0; i<n; i++)
+				{	
+					System.out.println("Enter Employee ID");
+					emp.setEmpId(scanner.nextInt());
+					if(EmpDatabase.isEmployeePresent(emp.getEmpId())) {
+						System.out.println("EmployeeID "+emp.getEmpId()+" is already present.");
+						continue;
+					}
+					System.out.println("Enter Employee name");
+					emp.setEmpName(scanner.next());
+
+					System.out.println("Enter Salary of Employee");
+					emp.setEmpSal(scanner.nextInt());
+
+					System.out.println("Enter Employee DOJ");
+					emp.setDoj(scanner.next());
+
+					System.out.println("Enter Department ID to which employee belongs to");
+					String deptId = scanner.next();
+
+					scanner.nextLine();
+					if(EmpDatabase.isDeptPresent(deptId))
+					{
+						emp.setDeptId(deptId);
+						EmpDatabase.addEmployee(emp);
+					}
+					else
+					{
+						System.out.println("Invalid Department");
+						System.out.println("Do you want to create a Department Y/N");
+						status = scanner.nextLine().charAt(0);
+						if(status == 'Y' || status == 'y' )
+						{
+							System.out.println("Enter Department name");
+							emp.setDeptName(scanner.next());
+							emp.setDeptId(deptId);
+							EmpDatabase.addDept(emp);
+
+							EmpDatabase.addEmployee(emp);
+						}
+					}
+				}
+				System.out.println("Do you want to return to main menu Y/N");
+				status = scanner.nextLine().charAt(0);
+				break;
+			}
+			case 2: {
+				System.out.println("How many records do you want to enter");
+				int n = scanner.nextInt();
+				for(int i=0; i<n; i++)
+				{
+					System.out.println("Enter Department name");
+					String name = scanner.next();
+
+					System.out.println("Enter Department ID");
+					String deptId = scanner.next();
+
+					scanner.nextLine();
+					if(isDeptPresent(deptId)) {
+						System.out.println("DeptID "+deptId+" is already exist");
+						continue;
+					}
+					emp.setDeptId(deptId);
+					emp.setDeptName(deptId);
+					addDept(emp);
+
+				}
+				System.out.println("Do you want to return to main menu Y/N");
+				status = scanner.nextLine().charAt(0);
+				break;
+			}
+			case 3:{
+				fetchEmpData();
+				System.out.println("Do you want to return to main menu Y/N");
+				status = scanner.nextLine().charAt(0);
+				break;
+
+			}
+			case 4:{
+				updateEmployee();
+				System.out.println("Do you want to return to main menu Y/N");
+				status = scanner.nextLine().charAt(0);
+				break;
+
+			}
+			case 5:{
+				empSalDetails();
+				System.out.println("Do you want to return to main menu Y/N");
+				status = scanner.nextLine().charAt(0);
+				break;
+
+			}
+			
+			case 6: {
+				EmpDatabase.close();
+				status='N';
+				break;
+			}
+			default:
+				System.out.println("Invalid Option");
+				System.out.println("Choose an option");
+			}
+			System.out.println();
+		}while(status == 'Y' || status == 'y');
+		scanner.close();
+		System.out.println("onnection closed");
+
+
+	}
+	
 	//connection to database
 	static void connectToDB() {
 		try {
@@ -26,6 +147,8 @@ public class EmpDatabase {
 			ex.printStackTrace();
 		}
 	}
+	
+	
 	//Adding new Employee record
 	static void addEmployee(Employee emp) {
 		String query="INSERT INTO EMP VALUES(?,?,?,?,?)";
@@ -42,6 +165,8 @@ public class EmpDatabase {
 			e.printStackTrace();
 		}	
 	}
+	
+	
 	//Adding new Department record
 	static void addDept(Employee emp) {
 		String query="INSERT INTO DEPT VALUES(?,?)";
@@ -55,17 +180,10 @@ public class EmpDatabase {
 			e.printStackTrace();
 		}
 	}
-	//closing the connection
-	static void close() {
-		try {
-			if(connection!=null) {
-				connection.close();
-				System.out.println("Connection closed");
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-	}
+	
+	
+		
+	
 	//checking whether the Employee ID is present or not
 	public static boolean isEmployeePresent(int empId) {
 		String query="SELECT* FROM EMP WHERE EMPID=?";
@@ -81,6 +199,8 @@ public class EmpDatabase {
 		return false;
 
 	}
+	
+	
 	//checking whether the Department ID is present or not
 	public static boolean isDeptPresent(String deptId) {
 		String query="SELECT* FROM DEPT WHERE DEPTID=?";
@@ -95,6 +215,8 @@ public class EmpDatabase {
 		}
 		return false;
 	}
+	
+	
 	//fetching the emp information along with dept name
 	public void fetchEmpData() {
 		String query="SELECT* FROM EMP,DEPT WHERE EMP.DEPTID=DEPT.DEPTID";
@@ -102,48 +224,50 @@ public class EmpDatabase {
 			ps=connection.prepareStatement(query);
 			ResultSet rs= ps.executeQuery();
 			while(rs.next()) {
-			System.out.println("empID is "+rs.getInt(1)+" and his/her name "+rs.getString(2)+",his/her salary"+rs.getDouble(3)+" "+rs.getString(4)+" "+ rs.getDate(5)+" "+rs.getString(7));
+				System.out.println("empID is "+rs.getInt(1)+" and his/her name "+rs.getString(2)+",his/her salary"+rs.getDouble(3)+" "+rs.getString(4)+" "+ rs.getDate(5)+" "+rs.getString(7));
 			}
-		
+
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-			}
 		}
-		
+		}
+	
+	
+		//updating emp record
 		public void updateEmployee() {
 		String query="UPDATE EMP set EMPNAME=?,EMPSAL=?,DEPTID=?,DOJ=? WHERE EMPID=?";
 		try {
-				ps=connection.prepareStatement(query);
-				
-				
-				System.out.println("Enter the employee id: ");
-				int empId = scanner.nextInt();
-				ps.setInt(5, empId);
-				
-				System.out.println("Enter the employee name to update: ");
-				String empName = scanner.next();
-				ps.setString(1, empName);
-				
-				System.out.println("Enter the employee salary to update: ");
-				double empSalary = scanner.nextDouble();
-				ps.setDouble(2, empSalary);
-		
-			System.out.println("Enter the department Id to update: ");
-				String deptId = scanner.next();
-				ps.setString(3, deptId);
-				
-				System.out.println("Enter the employee joining date to update: ");
-				String empJoiningDate = scanner.next();
-				ps.setString(4, empJoiningDate);
+			ps=connection.prepareStatement(query);
 
-				ps.executeUpdate();
-			
-				System.out.println("Updated successfully");
-				
-			   System.out.println("Employee Id: "+empId+" Employee Name: "+empName+"Employee Salary: "+empSalary+" Employee Joining date: "+empJoiningDate+" DeptId: "+deptId );
-			
-	           
+
+			System.out.println("Enter the employee id: ");
+			int empId = scanner.nextInt();
+			ps.setInt(5, empId);
+
+			System.out.println("Enter the employee name to update: ");
+			String empName = scanner.next();
+			ps.setString(1, empName);
+
+			System.out.println("Enter the employee salary to update: ");
+			double empSalary = scanner.nextDouble();
+			ps.setDouble(2, empSalary);
+
+			System.out.println("Enter the department Id to update: ");
+			String deptId = scanner.next();
+			ps.setString(3, deptId);
+
+			System.out.println("Enter the employee joining date to update: ");
+			String empJoiningDate = scanner.next();
+			ps.setString(4, empJoiningDate);
+
+			ps.executeUpdate();
+
+			System.out.println("Updated successfully");
+
+			System.out.println("Employee Id: "+empId+" Employee Name: "+empName+"Employee Salary: "+empSalary+" Employee Joining date: "+empJoiningDate+" DeptId: "+deptId );
+
+
 			} catch (SQLException e) {
 			
 				e.printStackTrace();
@@ -154,20 +278,35 @@ public class EmpDatabase {
 		
 		//query to find the employees count in each department whose salary greater than 30000 (deptName,EmployeesCount)
 		public void empSalDetails() {
-		String query="SELECT COUNT(EMPID) as EMPCOUNT ,DEPTID FROM EMP HAVING EMPSAL>30000 GROUPBY DEPTID";
+			String query="SELECT COUNT(EMP.EmpID) AS EMPLOYEE_COUNT,DEPT.DeptName FROM (DEPT INNER JOIN EMP ON EMP.DeptID =DEPT.DeptID) WHERE EMP.EmpSal> 30000 GROUP BY DEPT.DeptName;";
+
 			try {
 				ps=connection.prepareStatement(query);
 				ResultSet rs= ps.executeQuery();
 				while(rs.next()) {
-					System.out.println("Department ID is: "+rs.getString("DeptID"));
-					System.out.println("Number of employees in "+rs.getString("DeptID")+" department "+" whose salary greater than 30000: "+rs.getInt("EMPCOUNT"));
+					System.out.println("Department Name is: "+rs.getString("DeptName"));
+					System.out.println("Number of employees in "+rs.getString("DeptName")+" department "+" whose salary greater than 30000: "+rs.getInt("EMPLOYEE_COUNT"));
 				}
 			} catch (SQLException e) {
-				
-					e.printStackTrace();
+
+				e.printStackTrace();
+			}
+
+		}
+		//closing the connection
+		static void close() {
+			try {
+				if(connection!=null) {
+					connection.close();
+					System.out.println("Connection closed");
 				}
-				
-		}}					
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+
+		
+}					
 		
 		
 		
--- JDBC/src/com/bhavna/jdbc/Main.java
+++ JDBC/src/com/bhavna/jdbc/Main.java
@@ -12,122 +12,23 @@ import java.util.Scanner;
 //3.updating the employee table using jdbc
 //4. write a query for find the number of employees in each department  
 //whose employee sal  greater than 30000.(fetch coulms dept name,count of emplyee)
+
 public class Main {
 	public static void main(String[] args) {
-		EmpDatabase eb=new EmpDatabase();
-		EmpDatabase.connectToDB();
-		char status = 'Y';
-		Employee emp=new Employee();
+		EmpDatabase ed=new EmpDatabase();
+		
 		Scanner scanner = new Scanner(System.in);
-		eb.fetchEmpData();
-		do {
 			System.out.println("Welcome, Select an option");
 			System.out.println("1. Do you want to enter Employee record");
 			System.out.println("2. Do you want to enter Department record");
 			System.out.println("3. Fetch all Employee data along with dept details");
-			System.out.println("4. get emp count having sal>30000");
-			System.out.println("5. Close SQL Connection");
-			int option = scanner.nextInt();
-			scanner.nextLine();
-			switch (option) {
-			case 1: {
-				System.out.println("How many records do you want to enter");
-				int n = scanner.nextInt();
-				scanner.nextLine();
-				for(int i=0; i<n; i++)
-				{	
-				
-					
-					System.out.println("Enter Employee ID");
-					emp.setEmpId(scanner.nextInt());
-					if(EmpDatabase.isEmployeePresent(emp.getEmpId())) {
-						System.out.println("EmployeeID "+emp.getEmpId()+" is already present.");
-						continue;
-					}
-					System.out.println("Enter Employee name");
-					emp.setEmpName(scanner.next());
-					System.out.println("Enter Salary of Employee");
-					emp.setEmpSal(scanner.nextInt());
-					System.out.println("Enter Employee DOJ");
-					emp.setDoj(scanner.next());
-					System.out.println("Enter Department ID to which employee belongs to");
-					String deptId = scanner.next();
-					scanner.nextLine();
-					if(EmpDatabase.isDeptPresent(deptId))
-					{
-						emp.setDeptId(deptId);
-						EmpDatabase.addEmployee(emp);
-					}
-					else
-					{
-						System.out.println("Invalid Department");
-						System.out.println("Do you want to create a Department Y/N");
-						status = scanner.nextLine().charAt(0);
-						if(status == 'Y' || status == 'y' )
-						{
-							System.out.println("Enter Department name");
-							emp.setDeptName(scanner.next());
-							emp.setDeptId(deptId);
-							EmpDatabase.addDept(emp);
-							
-							EmpDatabase.addEmployee(emp);
-						}
-					}
-				}
-				System.out.println("Do you want to return to main menu Y/N");
-				status = scanner.nextLine().charAt(0);
-				break;
-			}
-			case 2: {
-				System.out.println("How many records do you want to enter");
-				int n = scanner.nextInt();
-				scanner.nextLine();
-				for(int i=0; i<n; i++)
-				{
-					System.out.println("Enter Department name");
-					String name = scanner.next();
-					System.out.println("Enter Department ID");
-					String deptId = scanner.next();
-					scanner.nextLine();
-					if(EmpDatabase.isDeptPresent(deptId)) {
-						System.out.println("DeptID "+deptId+" is already exist");
-						continue;
-					}
-					EmpDatabase.addDept(emp);
-					
-				}
-				System.out.println("Do you want to return to main menu Y/N");
-				status = scanner.nextLine().charAt(0);
-				break;
-			}
-			case 3:{
-				eb.fetchEmpData();
-				System.out.println("Do you want to return to main menu Y/N");
-				status = scanner.nextLine().charAt(0);
-				break;
-				
-			}
-			case 4:{
-				eb.empSalDetails();
-				System.out.println("Do you want to return to main menu Y/N");
-				status = scanner.nextLine().charAt(0);
-				break;
-				
-			}
-			case 5: {
-				EmpDatabase.close();
-				status='N';
-				break;
-			}
-			default:
-				System.out.println("Invalid Option");
-				System.out.println("Choose an option");
-			}
-			System.out.println();
-		}while(status == 'Y' || status == 'y');
-		scanner.close();
-		System.out.println("Thank you");
+			System.out.println("4. To update employee");
+			System.out.println("5. get emp count having sal>30000 ");
+			System.out.println("6. Close SQL Connection");
+			ed.selectByOption(scanner.nextInt());
 
+			
 }
 	}
+
 
